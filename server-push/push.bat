ba58e01d12cb29ae094305317bd48670d256af25
DrBracewell <dragonclawslice1@gmail.com>
2019-06-03 20:52:00 +0100

    Test for gitignore

--- server-push/push.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF 
-psftp.exe dexter@10.0.1.133 -pw Pand0ra1 -b "C:\Users\Dexter\Documents\Coding\virtual-ripred\server-push\commands.txt"
-echo y | plink.exe -ssh dexter@10.0.1.133 -pw Pand0ra1 "pm2 restart virtualRipred"
